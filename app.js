780a3500e11b97a6e3b905e566e589e40cf769b7
Vandrei <vandreiribeiro@gmail.com>
2025-08-17 15:54:38 -0300

    basic template engine setup

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const path = require("path");
+const hbs = require("express-handlebars");
 
 const PORT = 3000;
 const app = express();
 
+app.engine('handlebars', hbs.engine({}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-    res.send('<h1>Olá mundo</h1>');
+    res.render('home', {test: "Oioioioi"});
 })
 
 app.listen(PORT, () => {
