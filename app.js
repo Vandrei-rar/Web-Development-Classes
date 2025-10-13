4fe22b8f7571f96c8682584a42a96dd8663ee01d
Vandrei Ribeiro <vandrei.ribeiro@informa.solutions>
2025-10-13 19:06:51 -0300

    arquivos css e js criados

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
+const path = require('path');
 
 const PORT = 3000;
 const app = express();
 
 app.engine('handlebars', engine({ defaultLayout: 'main', extname: '.handlebars' }))
 app.set('view engine', 'handlebars')
+
+app.use(express.static(path.join(__dirname, 'css/')))
+app.use(express.static(path.join(__dirname, 'js/')))
 
 app.get('/', (req, res) => {
     res.render('./home', { nome: "Joãozinho", isLogged: true })
